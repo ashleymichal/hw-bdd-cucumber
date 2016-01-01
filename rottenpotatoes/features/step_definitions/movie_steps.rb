24fefeab7766027ada740b5987b734628cfb03c3
Ashley Michal <ashleymichal@gmail.com>
2016-01-01 17:01:43 -0600

    finished hw 3 part 2

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,11 +2,9 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
-    Movie.new(movie)
+    new_movie = Movie.new(:title => movie[:title], :rating => movie[:rating], :release_date => movie[:release_date])
+    new_movie.save
   end
-  # fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -15,18 +13,21 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  page.body.index(e1) < page.body.index(e2)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+When /I (un)?check the following ratings: "(.*)"/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(', ').each do |rating|
+    uncheck ? uncheck("ratings_#{rating}") : check("ratings_#{rating}")
+  end
+  # fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
