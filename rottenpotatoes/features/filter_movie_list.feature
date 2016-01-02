bb9fe1cf42093c3e866f3ea64f96e518483e5a57
Ashley Michal <ashleymichal@gmail.com>
2016-01-01 18:51:13 -0600

    Completed hw3

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -38,4 +38,5 @@ Scenario: restrict to movies with 'PG' or 'R' ratings
   And I should not see "Chicken Run"
 
 Scenario: all ratings selected
-  # see assignment
+  When I check the following ratings: "G, PG, PG-13, R, NC-17"
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,5 +1,3 @@
-# Add a declarative step here for populating the DB with movies.
-
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     new_movie = Movie.new(:title => movie[:title], :rating => movie[:rating], :release_date => movie[:release_date])
@@ -7,30 +5,16 @@ Given /the following movies exist/ do |movies_table|
   end
 end
 
-# Make sure that one string (regexp) occurs before or after another one
-#   on the same page
-
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
   page.body.index(e1) < page.body.index(e2)
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
-
 When /I (un)?check the following ratings: "(.*)"/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
   rating_list.split(', ').each do |rating|
     uncheck ? uncheck("ratings_#{rating}") : check("ratings_#{rating}")
   end
-  # fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  expect(page).to have_css("tr", count: 11)
 end
