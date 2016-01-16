349be41666e2db2912c44a20b2a4723dd9a7e3c5
Ashley Michal <ashleymichal@gmail.com>
2016-01-16 16:43:22 -0600

    finished search-by-director feature, broke sort order testing

--- rottenpotatoes/features/step_definitions/web_steps.rb
+++ rottenpotatoes/features/step_definitions/web_steps.rb
@@ -45,9 +45,8 @@ Given /^(?:|I )am on (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
-When /^(?:|I )go to (.+page)(?: sorted by "(.*)")?$/ do |page_name, sort_by|
+When /^(?:|I )go to (.+)?$/ do |page_name|
   visit path_to(page_name)
-  click_link(sort_by)
 end
 
 When /^(?:|I )press "([^"]*)"$/ do |button|
