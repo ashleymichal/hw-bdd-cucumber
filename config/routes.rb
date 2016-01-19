ead8618ba5c8460257acc20ac478b584825afca0
Ashley Michal <ashleymichal@gmail.com>
2016-01-19 17:02:35 -0600

    Completed ch 5

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rottenpotatoes::Application.routes.draw do
 
 	post '/movies/search_tmdb'
 	get '/movies/:id/similar_movies' => 'movies#similar_movies'
-  resources :movies
+  resources :movies do
+  	resources :reviews
+  end
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
 
